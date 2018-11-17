fcf0f4e1eb45bf260b5ec6b55c3500dfaa7d47d5
Jacques Kvam <jwkvam@gmail.com>
2018-11-16 21:25:56 -0800

    update

--- examples/sines.py
+++ examples/sines.py
@@ -17,7 +17,6 @@ camera = Camera()
 for i in np.linspace(0, 2 * np.pi, 128, endpoint=False):
     axes[0].plot(t, np.sin(t + i), color='blue')
     axes[1].plot(t, np.sin(t - i), color='blue')
-    plt.tight_layout()
     camera.snapshot(fig)
 
 animation = ArtistAnimation(fig, camera.pictures, interval=50, blit=True)
@@ -25,9 +24,7 @@ animation.save(
     'sines.mp4',
     dpi=100,
     savefig_kwargs={
-        # 'transparent': True,
         'frameon': False,
-        # 'bbox_inches': 'tight',
         'pad_inches': 'tight'
     }
 )
