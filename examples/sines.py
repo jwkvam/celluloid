4d8cf2919ce7dc7acf1785f4f4d7f62102fb7705
Jacques Kvam <jwkvam@gmail.com>
2018-11-17 23:21:04 -0800

    Update sines.py

--- examples/sines.py
+++ examples/sines.py
@@ -5,21 +5,15 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-from matplotlib.animation import ArtistAnimation
-
 from celluloid import Camera
-
-
-t = np.linspace(0, 2 * np.pi, 128, endpoint=False)
 
 fig, axes = plt.subplots(2)
 camera = Camera(fig)
-
-for i in np.linspace(0, 2 * np.pi, 128, endpoint=False):
+t = np.linspace(0, 2 * np.pi, 128, endpoint=False)
+for i in t:
     axes[0].plot(t, np.sin(t + i), color='blue')
     axes[1].plot(t, np.sin(t - i), color='blue')
     camera.snap()
-
 animation = camera.animate(interval=50, blit=True)
 animation.save(
     'sines.mp4',
